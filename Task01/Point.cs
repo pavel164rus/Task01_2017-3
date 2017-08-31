5074fcdb389df8b9ad10791a0d442c9e1c22b30c
Павел Филин <pavel164rus@mail.ru>
2017-08-31 11:38:29 +0400

    Добавьте файлы проекта.

---
+++ Task01/Point.cs
@@ -0,0 +1,23 @@
+﻿namespace Task01
+{
+    public class Point
+    {
+        public double X { get; set; }
+
+        public double Y { get; set; }
+
+        public double Z { get; set; }
+
+        public Point()
+        {
+
+        }
+
+        public Point(double x, double y, double z)
+        {
+            this.X = x;
+            this.Y = y;
+            this.Z = z;
+        }
+    }
+}
---
+++ Task01/Segment.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task01
+{
+    class Segment
+    {
+        public Point FirstPoint { get; set; }
+
+        public Point SecondPoint { get; set; }
+
+        public Segment(Point firstPoint, Point secondPoint)
+        {
+            this.FirstPoint = firstPoint;
+            this.SecondPoint = secondPoint;
+        }
+
+        public double Length => Math.Sqrt(Math.Pow(SecondPoint.X - FirstPoint.X, 2) + Math.Pow(SecondPoint.Y - FirstPoint.Y, 2) + Math.Pow(SecondPoint.Z - FirstPoint.Z, 2));
+
+        public double DistanceToSegment(Point point)
+        {
+            double DistanceAB = Length;
+            double DistanceAC = DistancePointToPoint(FirstPoint, point);
+            double DistanceBC = DistancePointToPoint(SecondPoint, point);
+
+            double semiperimeter = (DistanceAB + DistanceAC + DistanceBC) / 2;
+
+            double result = 2 * (Math.Sqrt(semiperimeter*(semiperimeter-DistanceAB)*(semiperimeter-DistanceAC)*(semiperimeter-DistanceBC))) / DistanceAB;
+            return result;
+        }
+
+        private double DistancePointToPoint(Point a, Point b)
+        {
+            return Math.Sqrt(Math.Pow(b.X - b.X, 2) + Math.Pow(b.Y - a.Y, 2) + Math.Pow(b.Z - a.Z, 2));
+        }
+    }
+}
